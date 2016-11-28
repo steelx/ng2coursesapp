eb848b20b97b0cf98776b3c03250c7003653fe28
Steel <ajinkya@riva.co>
2016-11-28 15:12:22 +0530

    ng2 firebase setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {initializeApp, database} from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor() {
+    // Initialize Firebase
+    let config = {
+      apiKey: "AIzaSyD9iohQt7IfvOohTy0btaotpoAGtbYdDFg",
+      authDomain: "ng2project01.firebaseapp.com",
+      databaseURL: "https://ng2project01.firebaseio.com",
+      storageBucket: "ng2project01.appspot.com",
+      messagingSenderId: "154189419076"
+    };
+    initializeApp(config);
+
+    let root = database().ref();
+
+    root.on('value', (snap) => {
+      console.log(snap.val());
+      
+    });
+
+  }
 }
