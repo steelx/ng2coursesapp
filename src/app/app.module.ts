da355fe4d75d13ff581277c32fb87a4adfd64c50
Steel <ajinkya@riva.co>
2016-11-29 13:06:54 +0530

    add new home component and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import {AngularFireModule} from 'angularfire2/index';
 import {firebaseConfig} from '../environments/firebase.config';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
