6d096bbb48d3c6f27e95b86fd144745bbad3ac33
Steel <ajinkya@riva.co>
2016-11-30 17:32:03 +0530

    indented safe commit

--- src/app/shared/model/lesson.ts
+++ src/app/shared/model/lesson.ts
@@ -1,23 +1,23 @@
 export class Lesson {
-    static fromJsonList(array): Lesson[] {
-        return array.map(Lesson.fromJson);
-    }
-    static fromJson({$key, description, duration, url, tags, pro, longDescription, courseId}): Lesson {
-        return new Lesson($key, description, duration, url, tags, pro, longDescription, courseId);
-    }
+  static fromJsonList(array): Lesson[] {
+    return array.map(Lesson.fromJson);
+  }
+  static fromJson({$key, description, duration, url, tags, pro, longDescription, courseId}): Lesson {
+    return new Lesson($key, description, duration, url, tags, pro, longDescription, courseId);
+  }
 
-    constructor(
-        public $key: string,
-        public description: string,
-        public duration: string,
-        public url: string,
-        public tags: string,
-        public pro: string,
-        public longDescription: string,
-        public courseId: string
-    ){}
+  constructor(
+    public $key: string,
+    public description: string,
+    public duration: string,
+    public url: string,
+    public tags: string,
+    public pro: string,
+    public longDescription: string,
+    public courseId: string
+  ){}
 
-    get isBeginner() {
-        return this.tags && this.tags.includes('BEGINNER');
-    }
+  get isBeginner() {
+    return this.tags && this.tags.includes('BEGINNER');
+  }
 }
